8e26d2c14476be0416ef8deb55616730c39e1d94
fahad <fahad.nizar@techversantinfo.com>
2022-09-29 11:16:57 +0530

    Commited

--- js/script1.js
+++ js/script1.js
@@ -3,7 +3,7 @@
     var sum = 0;
     var num=0;
     var randomNumber=0;
- 
+    var count=0;
     // Function to change the player name
     function editNames() {
         player1 = prompt("Change Player1 name");
@@ -16,18 +16,25 @@
                 "assets/face0.gif");
         setTimeout(function () {
             randomNumber = Math.floor(Math.random() * 6) + 1;
+			count++;
+		if (count%2==0){
+			playerA();
+		}
+		else {
+			playerB();
+		}
             document.querySelector(".img1").setAttribute("src",
                 "assets/face" + randomNumber + ".png");
     	    if (randomNumber==1) {
 		num++;
 		}
-start();
+
 
         } ,100);
     }
- function start() {
+ function playerA() {
 	if (num>0){
-		$(".lizard").remove();
+		$(".pig").remove();
 		sum+=randomNumber;
 		console.log(sum);
 		
@@ -35,101 +42,210 @@ start();
 		
 	 if (sum==5){
 		sum=35;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==23){
 		sum=42;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==9){
 		sum=51;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}else if (sum==36){
 		sum=5;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==49){
 		sum=7;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==48){
 		sum=86;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==56){
 		sum=8;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==62){
 		sum=83;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==69){
 		sum=91;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==87){
 		sum=66;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==82){
 		sum=20;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==95){
 		sum=38;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==100){
 		sum=100;
-		$('#'+sum+"").append('<img src="Assets/winner.gif" class="winner"/>');
+		$('#'+sum+"").append('<img src="Assets/pig2.gif" class="winner"/>');
 		
 		
 	}
 	else if (sum==100){
 		sum=100;
-		$('#'+sum+"").append('<img src="Assets/winner.gif" class="winner"/>');
+		$('#'+sum+"").append('<img src="Assets/pig2.gif" class="winner"/>');
 		
 		
 	}
 	else if (sum>100){
 		sum=sum-randomNumber;
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
 		
 		
 	}
 	else if (sum==100){
 		sum=100;
-		$('#'+sum+"").append('<img src="Assets/winner.gif" class="winner"/>');
+		$('#'+sum+"").append('<img src="Assets/pig2.gif" class="winner"/>');
 		
 		
 	}
 	else {
-		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="lizard"/>');
+		$('#'+sum+"").append('<img src="Assets/liz2.gif" class="pig"/>');
+		
+	}
+}
+}
+ function playerB() {
+	if (num>0){
+		$(".bird1").remove();
+		sum+=randomNumber;
+		console.log(sum);
+		
+	
+		
+	 if (sum==5){
+		sum=35;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==23){
+		sum=42;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==9){
+		sum=51;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}else if (sum==36){
+		sum=5;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==49){
+		sum=7;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==48){
+		sum=86;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==56){
+		sum=8;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==62){
+		sum=83;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==69){
+		sum=91;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==87){
+		sum=66;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==82){
+		sum=20;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==95){
+		sum=38;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==100){
+		sum=100;
+		$('#'+sum+"").append('<img src="Assets/bird3.gif" class="winner"/>');
+		
+		
+	}
+	else if (sum==100){
+		sum=100;
+		$('#'+sum+"").append('<img src="Assets/bird3.gif" class="winner"/>');
+		
+		
+	}
+	else if (sum>100){
+		sum=sum-randomNumber;
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
+		
+		
+	}
+	else if (sum==100){
+		sum=100;
+		$('#'+sum+"").append('<img src="Assets/bird3.gif" class="winner"/>');
+		
+		
+	}
+	else {
+		$('#'+sum+"").append('<img src="Assets/bird.png" class="bird1"/>');
 		
 	}
 }
